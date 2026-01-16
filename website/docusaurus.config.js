e9573d1865a1ee03744f29aa64927f1e99e4a905
Luigui Gallardo-Becerra <luiguimichelgallardo@gmail.com>
2026-01-16 09:31:14 -0800

    Change baseUrl for GitHub pages deployment

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://www.bioinfopalnorte.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/bioinfo_pal_norte/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
